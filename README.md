40314d62dc001a366b328656ad7ceccf6df972b3
Luciano Lima <luciano.lima@convergenceworks.com.br>
2015-03-15 22:23:20 -0300

    Add basic generator structure and files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Sencha Touch mobile generator with Cordova support
+# Sencha Touch mobile generator with Cordova support [![Build Status](https://secure.travis-ci.org/lucianomlima/generator-sencha-cordova.png?branch=master)](https://travis-ci.org/lucianomlima/generator-sencha-cordova)
 
 Yeoman generator that scaffolds out a Sencha Touch mobile app with Cordova support.
+
+## License
+
+Apache License 2.0
---
+++ app/index.js
@@ -0,0 +1,40 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  }
+});
